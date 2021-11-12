b4cf14c592cac046e6bd2e7e69b031df3c4d6ddd
private-yusuke <30387586+private-yusuke@users.noreply.github.com>
2021-11-12 09:53:33 +0900

    Move integration test to external script

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+benchmark_files=$(find . -name "*.smt2")
+benchmark_status_regex="(un)?sat"
+
+dub build
+
+for file in $benchmark_files; do
+    echo "$file"
+    actual=$(./smt-d --file $file)
+    status=$?
+    if [ $status -ne 0 ]; then
+        echo "An error occured while running smt-d, return code $status"
+        exit 2
+    fi
+    expected=$(cat "$file" | grep -oE "$benchmark_status_regex" | head -n 1)
+    if [ "$actual" != "$expected" ]; then
+        echo "Expected $expected, but got $actual"
+        exit 1
+    fi
+done
+
+echo "test succeeded"
